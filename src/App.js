a61dddc9f8fe6db15c7bc6bea1aaf46c3c8776ae
Erik <sunsink97@gmail.com>
2021-08-04 23:55:52 +0700

    First Push

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [currentPrice, setPrice] = useState([{ date: "", price: null }]);
+  function callAPI() {
+    fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+      .then((response) => response.json())
+      .then((response) => {
+        setPrice((arr) => [
+          ...arr,
+          { date: Date(), price: response.bpi.USD.rate },
+        ]);
+      });
+  }
+
+  function counter() {
+    console.log("counter start");
+    const i = 0;
+    // This block will be executed 100 times.
+    setInterval(function () {
+      if (i == 100) clearInterval(this);
+      else callAPI();
+    }, 60000);
+  } // End
+
+  console.log(currentPrice);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>bitcoin price per 1 minute</p>
+        <button onClick={counter}> Start tracking price </button>
+
+        <div>
+          {currentPrice.map((object) => (
+            <div>
+              <p>{object.price}</p>
+              <p>{object.date}</p>
+            </div>
+          ))}
+        </div>
       </header>
     </div>
   );
---
+++ src/Element.js
@@ -0,0 +1,6 @@
+import React from "react";
+function Element() {
+  return <div></div>;
+}
+
+export default Element;
